2afe6dc0c000d0e9fe61596d81fede4216438aa8
lubomir.angelov <lubomir.angelov.an@gmail.com>
2021-08-30 17:16:34 +0300

    add psycopg2 connection

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -7,6 +8,9 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = '3965a27bdf5ba755566e64e65c690aef'
     #SQLALCHEMY_DATABASE_URI = "postgres://irqtlwyphuuoqn:1833a784173005e27184c8b1f7232b5d9f9a8c033355b9cf2460dd52381faa09@ec2-50-17-255-244.compute-1.amazonaws.com:5432/dbbdtgt7l6mgm3"
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 class ProductionConfig(Config):
