4f06d606f63007cec7679434785c308218419223
lubomir.angelov <lubomir.angelov.an@gmail.com>
2021-08-30 20:14:10 +0300

    update init settings

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ login.login_view = 'login'
 app.config["SECRET_KEY"] = '3965a27bdf5ba755566e64e65c690aef'
 app.config['APP_SETTINGS']="config.ProductionConfig"
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ceramics:DYC42S3BVZjyrylfcCD0@localhost/GK_Pottery'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://irqtlwyphuuoqn:1833a784173005e27184c8b1f7232b5d9f9a8c033355b9cf2460dd52381faa09@ec2-50-17-255-244.compute-1.amazonaws.com:5432/dbbdtgt7l6mgm3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
