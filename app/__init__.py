cd0c1ce306e927555c5de910a4ddf1022db40b9b
lubomir.angelov <lubomir.angelov.an@gmail.com>
2021-08-30 20:22:32 +0300

    update init use environ do not use config

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
@@ -11,7 +12,9 @@ login.login_view = 'login'
 app.config["SECRET_KEY"] = '3965a27bdf5ba755566e64e65c690aef'
 app.config['APP_SETTINGS']="config.ProductionConfig"
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ceramics:DYC42S3BVZjyrylfcCD0@localhost/GK_Pottery'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://irqtlwyphuuoqn:1833a784173005e27184c8b1f7232b5d9f9a8c033355b9cf2460dd52381faa09@ec2-50-17-255-244.compute-1.amazonaws.com:5432/dbbdtgt7l6mgm3"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://irqtlwyphuuoqn:1833a784173005e27184c8b1f7232b5d9f9a8c033355b9cf2460dd52381faa09@ec2-50-17-255-244.compute-1.amazonaws.com:5432/dbbdtgt7l6mgm3"
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
