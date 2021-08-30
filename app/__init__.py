51a6590033dda8d80930de5e9f3c316c56f1d6f7
lubomir.angelov <lubomir.angelov.an@gmail.com>
2021-08-30 20:32:07 +0300

    use env var for secret key

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,8 @@ app = Flask(__name__)
 login = LoginManager(app)
 login.login_view = 'login'
 
-app.config["SECRET_KEY"] = '3965a27bdf5ba755566e64e65c690aef'
+app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
 app.config['APP_SETTINGS']="config.ProductionConfig"
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ceramics:DYC42S3BVZjyrylfcCD0@localhost/GK_Pottery'
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://irqtlwyphuuoqn:1833a784173005e27184c8b1f7232b5d9f9a8c033355b9cf2460dd52381faa09@ec2-50-17-255-244.compute-1.amazonaws.com:5432/dbbdtgt7l6mgm3"
-#app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace("postgres", "postgresql")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
--- app/config.py
+++ app/config.py
@@ -6,10 +6,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = '3965a27bdf5ba755566e64e65c690aef'
-    #SQLALCHEMY_DATABASE_URI = "postgres://irqtlwyphuuoqn:1833a784173005e27184c8b1f7232b5d9f9a8c033355b9cf2460dd52381faa09@ec2-50-17-255-244.compute-1.amazonaws.com:5432/dbbdtgt7l6mgm3"
+    SECRET_KEY = os.environ['SECRET_KEY']
     DATABASE_URL = os.environ['DATABASE_URL']
-    #SQLALCHEMY_DATABASE_URI = "postgresql://ceramics:DYC42S3BVZjyrylfcCD0@localhost/GK_Pottery"
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
