e141dcf94cfaf21ed3c09a1415067c16a80bd487
lyubomir angelov <lubomir.angelov.an@gmail.com>
2023-11-22 06:39:42 +0200

    ÃÃadd new alembic migrations

---
+++ alembic/versions/36e814539f0d_add_sequence_to_fragments_table.py
@@ -0,0 +1,24 @@
+"""add sequence to fragments table
+
+Revision ID: 36e814539f0d
+Revises: 
+Create Date: 2023-11-22 06:33:34.523058
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '36e814539f0d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
